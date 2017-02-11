77132484ba6737231fd2f7881f1f7552dc253b53
Brandon Schmuck <bschmuck@brandons-macbook-4.local>
2017-02-10 20:08:01 -0500

    Initial Commit

---
+++ Pigment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pigment
+//
+//  Created by Brandon Schmuck on 2/10/17.
+//  Copyright © 2017 Brandon Schmuck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
